3a6ad1b7246d4d956c4a96a8e4fcb533a61b733c
zeena kareem <zena34269@gmail.com>
2020-08-07 17:58:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,24 +7,5 @@ class Formatter {
  return  name.replace(/[^A-Za-z-d-\s']+/g, '')
   }
   static titleize(name) {
-    let text=''
-   let a=name.split(' ')
-   console.log(a)
-   for (let i=0; i <a.length; ++i)
-{ if( a[i]!== 'a'||'an'|| 'but'|| 'of'|| 'and'|| 'for'|| 'at'|| 'by'||'from')
-{ 
-  if(i==a.length-1){
-    text += a[i].charAt(0).toUpperCase()+a[i].slice(1);
    
-  }
-  else{
-     text += a[i].charAt(0).toUpperCase()+a[i].slice(1)+' ';
-  }
-  
-}
-  else{
-    text +=a[i];
-  }
-
-return text
 }
