5abf1dddeb443642f102313059a1f1666f0cb02b
zeena kareem <zena34269@gmail.com>
2020-08-07 18:00:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ class Formatter {
   }
   static titleize(string) {
     let words = string.split(' ');
+    
     for (let n = 0; n < words.length; n++) {
       words[n] = words[n].charAt(0).toUpperCase() + words[n].slice(1);
     }
